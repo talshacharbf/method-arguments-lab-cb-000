21a1c366a8496d60330cf4e150f3a605b4e61809
tal shachar <tal.shachar@buzzfeed.com>
2017-12-27 00:19:56 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,7 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
 
-def introduction(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and am learning to program in #{language}."
 end
